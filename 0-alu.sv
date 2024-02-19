2a029a6b02f023335c2c883bbedfc623c3caf26e
Iliass Lasri <iliass.lasri@telecom-paris.fr>
2024-02-19 15:23:14 +0100

    ALU and ALUDecoder

---
+++ 0-alu.sv
@@ -0,0 +1,20 @@
+/**** This file is tha ALU module. It is responsible for performing the arithmetic and logical operations. ****/
+/// Written by: Iliass Lasri
+
+module ALU(
+    input [31:0] SrcA,
+    input [31:0] SrcB,
+    input [2:0] ALUControl, // ALUControl will come from the ALU Decoder file 00-aludec.sv
+    output reg [31:0] ALUResult,
+);
+
+always_comb
+    case(ALUControl)
+        3'b000: ALUResult = SrcA + SrcB; // addition
+        3'b001: ALUResult = SrcA - SrcB; // subtraction
+        3'b010: ALUResult = SrcA & SrcB; // and
+        3'b011: ALUResult = SrcA | SrcB; // or
+        3'b101: ALUResult = (SrcA < SrcB) ? 32'b1 : 32'b0; // set less then
+        default: ALUResult = '0; // ????
+    endcase
+endmodule
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # RISC-V implementation verilog
+
+    This is the resord of implemetation of RISC-V in verilog. The project is based on the book "Computer Organization and Design RISC-V Edition" by David A. Patterson and John L. Hennessy.
+
+    The project is a single cycle RISC-V implementation. 
+
+    Let's start with the basic components of the RISC-V architecture.
+
+    First let's implement the basic components of the RISC-V architecture. The basic components are:
+    1. ALU
+    
